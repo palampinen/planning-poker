911701ddc849b9035ea49c9c2f0b1c44286ab38c
Pasi Lampinen <pasi.lampinen@futurice.com>
2017-02-23 18:30:01 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+$(function() {
+  FastClick.attach(document.body);
+
+  function fullscreen(element) {
+    // $(element.target).toggleClass('fullscreen');
+    var elem = element.currentTarget;
+    if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else {
+      $(element.target).toggleClass('fullscreen');
+    }
+
+  }
+
+  $('.card').click(fullscreen)
+});
